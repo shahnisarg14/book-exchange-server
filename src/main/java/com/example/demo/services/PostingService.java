6525ea4c8d3697554472766dd9da96b3e6656f4a
Nisarg Shah <shahnisarg087@gmail.com>
2019-04-16 13:22:13 -0400

    Edit posting

--- src/main/java/com/example/demo/services/PostingService.java
+++ src/main/java/com/example/demo/services/PostingService.java
@@ -1,5 +1,6 @@
 package com.example.demo.services;
 
+import com.example.demo.model.Book;
 import com.example.demo.model.Posting;
 import com.example.demo.repository.PostingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,11 +30,35 @@ public class PostingService {
         return postingRepository.findPostsByUserName(username);
     }
 
+    @GetMapping("/api/my-posting/{pId}")
+    public Posting getPostingById(@PathVariable("pId") int pId) {
+        Optional<Posting> posting = postingRepository.findById(pId);
+        if (posting.isPresent())  return posting.get();
+        else throw new NoSuchElementException();
+    }
+
     @PostMapping("/api/my-posting")
     public Posting createMyPosting(@RequestBody Posting newMyPosting){
         postingRepository.save(newMyPosting);
         return newMyPosting;
     }
 
+    @PutMapping("/api/posting/{pId}")
+    public Posting updatePosting(@PathVariable("pId") int pId,@RequestBody Posting updatedPosting) {
+        Optional<Posting> posting = postingRepository.findById(pId);
+        if(posting.isPresent())
+        {
+            Posting oldPosting = posting.get();
+            oldPosting.setBook(updatedPosting.getBook());
+            postingRepository.save(oldPosting);
+            return oldPosting;
+        }
+        return null;
+    }
 
+    @DeleteMapping("/api/posting/{pId}")
+    public List<Posting> deletePosting(@PathVariable("pId") int pId) {
+        postingRepository.deleteById(pId);
+        return getAllPostingsOfUser(getPostingById(pId).getUser().getUsername());
+    }
 }
